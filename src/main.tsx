1e2834db8248c906344dc418286467b09a4daf0f
Guilherme Bermêo <gm.bermeo@hotmail.com>
2022-07-13 17:21:59 -0300

    🎨 Readme and organization.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./css/index.css";
+import "./Styles/index.css";
+import "./Styles/App.css";
+import "./Styles/ArtDeco.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
 );
